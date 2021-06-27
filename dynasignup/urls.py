1d24b05341c63f7dc65624cafd20a904f23aec03
Augustin Borsu <a.borsu@acelpb.com>
2021-06-27 09:09:58 +0200

    fix: whitenoise needs static route

--- dynasignup/urls.py
+++ dynasignup/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("signup.urls")),
-    path('accounts/', include('django_registration.backends.one_step.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('admin/', admin.site.urls),
-]
+                  path("", include("signup.urls")),
+                  path('accounts/', include('django_registration.backends.one_step.urls')),
+                  path('accounts/', include('django.contrib.auth.urls')),
+                  path('admin/', admin.site.urls),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
