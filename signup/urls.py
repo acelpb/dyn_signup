3a03d978d562d02dacf9a76738602cdc6356a15b
Augustin Borsu <a.borsu@acelpb.com>
2022-05-01 12:33:43 +0200

    chore: remove 2021 signup

--- signup/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import ParticpantFormView, BalladView, ParticipantReviewView
-
-urlpatterns = [
-    path("", BalladView.as_view(), name="ballad_list"),
-    path("review", ParticipantReviewView.as_view(), name="participant_view"),
-    # path("submit", ParticpantFormView.as_view(), name="participant_form"),
-]
