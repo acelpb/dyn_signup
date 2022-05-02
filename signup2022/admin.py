cf56b8db01ceb21b58cb774e61056fc4d43925fc
Augustin Borsu <a.borsu@acelpb.com>
2022-05-02 22:54:56 +0200

    feat: show vae field in admin

--- signup2022/admin.py
+++ signup2022/admin.py
@@ -10,7 +10,7 @@ class ParticipantInfoInline(admin.StackedInline):
     model = Participant
     extra = 0
     fields = (
-        "first_name", "last_name", "email", "birthday", "city", "country"
+        "first_name", "last_name", "email", "birthday", "city", "country", "vae"
     )
 
 class ParticipantDaysInline(admin.TabularInline):
