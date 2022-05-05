d8f1904eb9389951e34d24aab2ab2378d61a9dd4
Augustin Borsu <a.borsu@acelpb.com>
2022-05-05 21:36:12 +0200

    feat: add participant filter

--- signup2022/admin.py
+++ signup2022/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.admin import SimpleListFilter
 
 from .models import Participant, Signup, Bill
 
@@ -12,6 +13,7 @@ class ParticipantInfoInline(admin.StackedInline):
     fields = (
         "first_name", "last_name", "email", "birthday", "city", "country", "vae"
     )
+
 
 class ParticipantDaysInline(admin.TabularInline):
     verbose_name = "Présences Participants"
@@ -35,10 +37,62 @@ class SignupAdmin(admin.ModelAdmin):
     inlines = [ParticipantInfoInline, ParticipantDaysInline]
 
 
+class SignupStatusFilter(SimpleListFilter):
+    title = "Statut de l'inscription"
+    parameter_name = "statut"  # you can put anything here
+
+    def lookups(self, request, model_admin):
+        # This is where you create filter options; we have two:
+        return [
+            ("on_hold", "On Hold"),
+            ("on_hold_vae", "On Hold VAE"),
+            ("partial", "On Hold Partial"),
+            ("validated", "Confirmé"),
+            ("payed", "Payé"),
+        ]
+
+    def queryset(self, request, queryset):
+        filters = {
+            "on_hold": {'signup_group__on_hold': True},
+            "on_hold_vae": {'signup_group__on_hold_vae': True},
+            "partial": {'signup_group__on_hold_partial': True},
+            "validated": {
+                'signup_group__validated_at__isnull': False,
+                'signup_group__on_hold': False,
+            },
+            "payed": {'signup_group__bill__balance': -1},  # TODO no one has payed yet.
+            None: {},
+        }
+        return queryset.distinct().filter(**filters[self.value()])
+
+
+class SignupDayFilter(SimpleListFilter):
+    title = "Participant au jour"
+    parameter_name = "date"
+
+    def lookups(self, request, model_admin):
+        # This is where you create filter options; we have two:
+        return [
+            ("d2022_07_18", "18 Juillet"),
+            ("d2022_07_19", "19 Juillet"),
+            ("d2022_07_20", "20 Juillet"),
+            ("d2022_07_21", "21 Juillet"),
+            ("d2022_07_22", "22 Juillet"),
+            ("d2022_07_23", "23 Juillet"),
+            ("d2022_07_24", "24 Juillet"),
+            ("d2022_07_25", "25 Juillet"),
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.distinct().filter(**{self.value(): True})
+
+
 @admin.register(Participant)
 class ParticipantAdmin(admin.ModelAdmin):
     list_display = (
         'id',
+        "signup_group",
         'first_name',
         'last_name',
         'd2022_07_18',
@@ -51,15 +105,10 @@ class ParticipantAdmin(admin.ModelAdmin):
         'd2022_07_25',
         "vae",
     )
+    search_fields = ["first_name", "last_name"]
     list_filter = (
-        'd2022_07_18',
-        'd2022_07_19',
-        'd2022_07_20',
-        'd2022_07_21',
-        'd2022_07_22',
-        'd2022_07_23',
-        'd2022_07_24',
-        'd2022_07_25',
+        SignupStatusFilter,
+        SignupDayFilter,
         "vae",
     )
 
